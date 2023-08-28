6390ba638b0ebbff1ac69040aaabf71d04664e65
Alisher <alisherotajonov866@gmail.com>
2023-08-28 17:10:02 +0500

    create log in interface

---
+++ app/src/main/java/com/example/a1pms_mobile_app/login/LogIn.kt
@@ -0,0 +1,40 @@
+package com.example.a1pms_mobile_app.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.makeText
+import androidx.navigation.fragment.findNavController
+import com.example.a1pms_mobile_app.R
+import com.example.a1pms_mobile_app.databinding.FragmentLogInBinding
+class LogIn : Fragment() {
+
+    private var _binding: FragmentLogInBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentLogInBinding.inflate(inflater, container, false)
+
+        binding.btnLogIn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginIn_to_dashboard)
+        }
+        binding.btnForgotPassword.setOnClickListener {
+            makeText(requireContext(), "Clickable", Toast.LENGTH_LONG).show()
+        }
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
